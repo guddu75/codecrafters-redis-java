fc461f89d7d17aa6e9dae370d0ba35d67c7e5f72
Debopriyo <guddumajumder75@gmail.com>
2024-02-06 01:53:54 +0530

    multiple ping request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,9 +20,13 @@ public class Main {
           clientSocket = serverSocket.accept();
           PrintWriter out =  new PrintWriter(clientSocket.getOutputStream());
           BufferedReader in =  new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
-          out.println("+PONG\r");
-          out.flush();
-            
+          String str;
+          while((str = in.readLine()) != null){
+            if(str.equals("ping")){
+                out.print("+PONG\\r\\n");
+                out.flush();
+            }
+          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
