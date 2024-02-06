17505b79916cebad85de7250c0eebdee10ccf83d
Debopriyo <guddumajumder75@gmail.com>
2024-02-07 01:14:14 +0530

    concurrent client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,38 +6,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+
+    private static void handleClient( Socket clientSocket) throws  IOException{
+        PrintWriter out =  new PrintWriter(clientSocket.getOutputStream());
+        BufferedReader in =  new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+        String str;
+        while((str = in.readLine()) != null){
+            System.out.println(str);
+            if(str.equals("ping")){
+                out.print("+PONG\r\n");
+                out.flush();
+            }
+        }
+        clientSocket.close();
+    }
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+//        Socket clientSocket = null;
         int port = 6379;
         try {
 //            System.out.println("Hello world!");
           serverSocket = new ServerSocket(port);
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          PrintWriter out =  new PrintWriter(clientSocket.getOutputStream());
-          BufferedReader in =  new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
-          String str;
-          while((str = in.readLine()) != null){
-            if(str.equals("ping")){
-                out.print("+PONG\r\n");
-                out.flush();
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                new Thread(() -> {
+                    try{
+                        handleClient(clientSocket);
+                    }catch (IOException e){
+                        System.out.println("IOException: " + e.getMessage());
+                    }
+
+                }).start();
             }
-          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
+//          try {
+//            if (clientSocket != null) {
+//              clientSocket.close();
+//            }
+//          } catch (IOException e) {
+//            System.out.println("IOException: " + e.getMessage());
+//          }
         }
 //      System.out.println("Here i am");
   }
+
+
 }
