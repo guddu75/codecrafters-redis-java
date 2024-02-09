5da6d2fbb0b43a1e060e1a698e85954d6ca8eeb2
Debopriyo <guddumajumder75@gmail.com>
2024-02-09 21:27:51 +0530

    get and set 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,7 +31,7 @@ public class Main {
                     String key = arr.get(4);
                     String value = arr.get(6);
                     database.set(key,value);
-                    out.print("OK\r\n");
+                    out.print("+OK\r\n");
                     out.flush();
                 }else if(cmd.toLowerCase().contentEquals("get")){
                     String key = arr.get(4);
